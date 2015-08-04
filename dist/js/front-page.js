bcf017c6aed4d45d0c3f7e6ca03975288adafb30
Felix Ekness <felixekness@gmail.com>
2015-08-04 17:46:08 -0500

    Added group photo to people's tab

--- dist/js/front-page.js
+++ dist/js/front-page.js
@@ -3,7 +3,7 @@ $( window ).load(function() {
     // Splash banner sticky variables
     var splash_height = $('.splash').outerHeight();
     var nav_height = $('.navbar').outerHeight();
-    var splash_from_top = $('.navbar').outerHeight(true) + 
+    var splash_from_top = $('.navbar').outerHeight(true) +
                           $('.banner-placement').outerHeight(true);
     var slide = true;
 
@@ -24,7 +24,7 @@ $( window ).load(function() {
     // Splash banner sticks below nav when the splash banner is scrolled past the nav
     //  Will not stick splash banner on mobile or when screen size is reduced to mobile size
     $(window).scroll(function() {
-        if ($(window).width() > 768 || !slide) { 
+        if ($(window).width() > 768 || !slide) {
             if (($(window).scrollTop() >= (splash_from_top - nav_height)) && slide) {
                 $('.splash').addClass('sticky');
             	$('.text').animate({marginTop: "+=" + splash_height}, 0);
